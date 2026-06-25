64005c0239c0154b33e3871086123a0c64dc11bf
miguelquintanilhapy <miguelquintanilha61@gmail.com>
2026-06-25 14:19:00 -0300

    feat: configurando novos models

--- magal/Models/AnoCalendario.cs
+++ magal/Models/AnoCalendario.cs
@@ -1,10 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace magal.Models
 {
-    internal class AnoCalendario
+    public class AnoCalendario : BaseModel
     {
+        private int _idAnoCalendario;
+        private int _ano;
+        private decimal _horasDia;
+        private DateTime? _inicioFerias;
+        private DateTime? _fimFerias;
+
+        public int id_ano_calendario
+        {
+            get => _idAnoCalendario;
+            set { _idAnoCalendario = value; OnPropertyChanged(); }
+        }
+
+        public int ano
+        {
+            get => _ano;
+            set { _ano = value; OnPropertyChanged(); }
+        }
+
+        public decimal horas_dia
+        {
+            get => _horasDia;
+            set { _horasDia = value; OnPropertyChanged(); }
+        }
+
+        public DateTime? inicio_ferias
+        {
+            get => _inicioFerias;
+            set { _inicioFerias = value; OnPropertyChanged(); }
+        }
+
+        public DateTime? fim_ferias
+        {
+            get => _fimFerias;
+            set { _fimFerias = value; OnPropertyChanged(); }
+        }
+
+        public override string ToString() => ano.ToString();
     }
 }
--- magal/Models/EventoCalendario.cs
+++ magal/Models/EventoCalendario.cs
@@ -1,10 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace magal.Models
 {
-    internal class EventoCalendario
+    public class EventoCalendario : BaseModel
     {
+        private int _idEvento;
+        private int _idAnoCalendario;
+        private DateTime _dataOriginal;
+        private DateTime _dataObservada;
+        private string _descricao = string.Empty;
+        private string _tipo = string.Empty;
+
+        public int id_evento
+        {
+            get => _idEvento;
+            set { _idEvento = value; OnPropertyChanged(); }
+        }
+
+        public int id_ano_calendario
+        {
+            get => _idAnoCalendario;
+            set { _idAnoCalendario = value; OnPropertyChanged(); }
+        }
+
+        public DateTime data_original
+        {
+            get => _dataOriginal;
+            set
+            {
+                _dataOriginal = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsSubstituido));
+                OnPropertyChanged(nameof(DescricaoCompleta));
+            }
+        }
+
+        public DateTime data_observada
+        {
+            get => _dataObservada;
+            set
+            {
+                _dataObservada = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsSubstituido));
+                OnPropertyChanged(nameof(DescricaoCompleta));
+            }
+        }
+
+        public string descricao
+        {
+            get => _descricao;
+            set
+            {
+                _descricao = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(DescricaoCompleta));
+            }
+        }
+
+        public string tipo
+        {
+            get => _tipo;
+            set { _tipo = value; OnPropertyChanged(); }
+        }
+
+        public bool IsSubstituido => data_original.Date != data_observada.Date;
+
+        public string DescricaoCompleta => IsSubstituido
+            ? $"{descricao} ({data_original:d/M} Substituído por {data_observada:d/M})"
+            : descricao;
     }
 }
